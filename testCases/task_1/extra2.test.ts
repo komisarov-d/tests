abe676da42309b1c4f160bc94d285a6ebe9b7d09
komisarov-d <komisarov.dev@gmail.com>
2021-07-22 10:22:30 +0300

    Added aside tests

---
+++ testCases/task_1/extra2.test.ts
@@ -0,0 +1,43 @@
+// import supertest from 'supertest';
+// import app from '../../../src/app';
+// import { ResponseStatusCode } from '../../../src/common/enums'
+//;import { insert } from '../../database';
+// import { getApiV1Route } from '../../helpers';
+// import { BookingRoomRoute } from '../../common/enums';
+// import { insert } from '../../database';
+// import { DbTableName } from "../../common/enums";
+
+// import { getValidCreateUserPayload } from './helpers';
+
+// const request = supertest(app);
+
+describe("Task 1", () => {
+  // const registerEndpoint = getApiV1Route(AuthRoute.Register);
+  describe("Task 1 description", () => {
+    test("Task 1 test #1", async () => {
+      // const payload = getValidCreateUserPayload('testMail@gmail.com');
+      // const response = await request.post(registerEndpoint).send(payload);
+      // expect(response.status).toEqual(ResponseStatusCode.Ok);
+    });
+    test("Task 1 test #2", async () => {
+      // const payload = getValidCreateUserPayload('testMail@gmail.com');
+      // const response = await request.post(registerEndpoint).send(payload);
+      // expect(response.status).toEqual(ResponseStatusCode.Ok);
+    });
+    test("Task 1 test #3", async () => {
+      // const payload = getValidCreateUserPayload('testMail@gmail.com');
+      // const response = await request.post(registerEndpoint).send(payload);
+      // expect(response.status).toEqual(ResponseStatusCode.Ok);
+    });
+  });
+});
+test("Task 1 test #4 extra", async () => {
+  // const payload = getValidCreateUserPayload('testMail@gmail.com');
+  // const response = await request.post(registerEndpoint).send(payload);
+  // expect(response.status).toEqual(ResponseStatusCode.Ok);
+});
+test("Task 1 test #5 extra", async () => {
+  // const payload = getValidCreateUserPayload('testMail@gmail.com');
+  // const response = await request.post(registerEndpoint).send(payload);
+  // expect(response.status).toEqual(ResponseStatusCode.Ok);
+});
